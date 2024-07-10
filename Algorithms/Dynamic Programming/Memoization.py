3556b9726a58a944318dc707f7cc0634303dd22e
Howard Sun <howardsun@pm.me>
2024-07-10 19:10:22 -0400

    memoization

--- Algorithms/Dynamic Programming/Memoization.py
+++ Algorithms/Dynamic Programming/Memoization.py
@@ -1,4 +1,5 @@
 import time, random
+from functools import lru_cache
 
 times = []
 
@@ -13,3 +14,39 @@ for i in range(len(array)):
     print(square_no_memo(array[i]))
 t2 = time.time()
 times.append(t2 - t1)
+
+cache = {}
+
+
+def square_with_memo(num):
+    if num in cache:
+        return cache[num]
+    else:
+        cache[num] = num**2
+        return cache[num]
+
+
+t1 = time.time()
+for i in range(len(array)):
+    print(square_with_memo(array[i]))
+t2 = time.time()
+times.append(t2 - t1)
+
+
+@lru_cache(maxsize=10000)
+def squaring(number):
+    return number**2
+
+
+print(array)
+t1 = time.time()
+for i in range(len(array)):
+    print(squaring(array[i]))
+t2 = time.time()
+times.append(t2 - t1)
+
+print(times)
+print(cache)
+
+
+# print(squaring.cache_info())
