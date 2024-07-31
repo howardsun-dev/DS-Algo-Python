21ae27efabe8000e43f72f9450d0bc52d0a2c770
Howard Sun <howardsun@pm.me>
2024-07-31 00:47:39 -0400

    sorting in python

---
+++ pythonPractice/strings.py
@@ -0,0 +1,16 @@
+# Strings are similar to arrays
+
+s = "abc"
+# Sliced from index 0 to index 2, but inclusive
+print(s[0:2])
+
+# Strings are immutable, but can be updated, anytime string is modified it is O(n)
+# Since it creates a new string
+s += "def"
+print(s)
+
+# Valid numeric strings can be converted
+print(int("123") + int("123"))
+
+# Numbers can be converted into strings
+print(str(123) + str(123))
