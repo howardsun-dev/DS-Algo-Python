cd172fe66de3155993636b7c5bffebe77b5eded4
Howard Sun <howardsun@pm.me>
2024-07-31 00:16:19 -0400

    sorting in python

--- pythonPractice/sorting.py
+++ pythonPractice/sorting.py
@@ -2,3 +2,24 @@
 arr = [5, 4, 7, 3, 8]
 arr.sort()
 print(arr)
+
+# Sort in reverse order/descending
+arr.sort(reverse=True)
+print(arr)
+
+# Defaults to alphabetical
+arr = ["bob", "alice", "jane", "doe"]
+arr.sort()
+print(arr)
+
+# Custom sort (by string len)
+arr.sort(key=lambda x: len(x))
+print(arr)
+
+# List comprehension
+arr = [i + i for i in range(5)]
+print(arr)
+
+# 2-D Lists
+arr = [[0] * 4 for i in range(4)]
+print(arr)
