3ef3d89c6d998efa867f5eceaf2c8495dfad351e
Howard Sun <howardsun@pm.me>
2024-10-10 12:36:45 -0400

    Finished is_anagram for Python

--- Algorithms/neetcode/Is_Anagram.py
+++ Algorithms/neetcode/Is_Anagram.py
@@ -1,11 +1,22 @@
 # Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false;
 # An anagram is a string that contains the exact same characeters as another string, but th eorder of the charactrs can be different
 
-
-
-from typing import List
+from collections import Counter
 
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
+        # Count character in both strings
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+
+        # Compare retults
+        return counter_s == counter_t
+
+
+solution = Solution()
+
+s = "racecar"
+t = "carrace"
+
+print(solution.isAnagram(s, t))
